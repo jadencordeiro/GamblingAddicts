1eae8a9c0d3da25b4279946cae851c022ca6a7c0
zilongzhao <davidzilongzhao@outlook.com>
2023-11-27 12:03:33 -0500

    added Javadoc to Bet and Wallet. Created Test files for them.

--- src/main/java/user/entity/Wallet.java
+++ src/main/java/user/entity/Wallet.java
@@ -4,6 +4,10 @@ import bet.Bet;
 
 import java.util.ArrayList;
 
+/**
+ * @param: balance The balance in the wallet.
+ * @param: bets The list of bets inside this wallet.
+ */
 public class Wallet {
 
     private float balance;
@@ -34,7 +38,11 @@ public class Wallet {
     }
     // shall we add a precondition that addition >= 0 here, so we could omit the "if" part?
 
-    // Add bet is giving an error because bet isn't implemented yet
+    /**
+     * @param bet Takes a Bet for input.
+     * @see java.bet.Bet
+     * @throws InsufficientFundsException throw Exception when the fund is not enough for the bet place.
+     */
     public void addBet(Bet bet) throws InsufficientFundsException {
         if ((this.balance -= bet.getWager()) < 0) {
             throw new InsufficientFundsException();
@@ -49,6 +57,10 @@ public class Wallet {
         }
     }
 
+
+    /**
+     * payout each individual bet inside the bets list in this wallet.
+     */
     public void settlement() {
         for (Bet bet : bets) {balance += bet.payout();}
     }
