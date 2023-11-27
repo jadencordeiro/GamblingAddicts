595d257fab80382f6c8c5928e1ab5a0dafae69bb
Maforikan Amoussou <maforikan.amoussou@mail.utorontoc.a>
2023-11-27 09:26:41 -0500

    Using Java Swing, a basic outline fo the code for the profile page. A field for wallet history which will include active and recent bets, as wella s any other transactions that may have taken place in wallet, ie a user adding or removing funds from their wallet. The text fields currently in the code represent an aspect of what each User has in terms of their wallet. I think their wallet id should also be included in this page.

---
+++ src/main/java/user/view/ProfilePageView.java
@@ -0,0 +1,65 @@
+package user.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ProfilePageView extends JFrame {
+    private JTextField usernameField;
+    private JTextField walletField;
+    private JTextField balanceField;
+    private JTextField betHistoryField;
+    private JButton saveButton;
+
+    public ProfilePageView() {
+        setTitle("Profile Page");
+        setSize(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+
+        setLayout(new GridLayout(3, 2));
+
+
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameField = new JTextField();
+
+        JLabel walletLabel = new JLabel("Wallet:");
+        walletField = new JTextField();
+
+        saveButton = new JButton("Save");
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveProfile();
+            }
+        });
+
+        add(usernameLabel);
+        add(usernameField);
+        add(walletLabel);
+        add(walletField);
+        add(saveButton);
+
+
+        setVisible(true);
+    }
+
+    private void saveProfile() {
+        String username = usernameField.getText();
+        String wallet = walletField.getText();
+
+        JOptionPane.showMessageDialog(this, "Profile saved successfully!");
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ProfilePageView();
+            }
+        });
+    }
+}
+
